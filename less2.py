6f61f5b725a8285fa8f3e2919ad1b1556e132813
mulenRouge <mulenrouge28@yandex.ru>
2022-11-26 11:18:10 +0300

    DZ1

---
+++ less2.py
@@ -0,0 +1,29 @@
+# Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости,
+# в которой находится эта точка (или на какой оси она находится).
+# Пример:
+# - x=34; y=-30 -> 4
+# - x=2; y=4-> 1
+# - x=-34; y=-30 -> 3
+def input_koordinate():
+    x, y = map(int, input("Введите X,Y через пробел: ").split())
+    return x, y
+
+
+x, y = input_koordinate()
+
+
+def checkCoordinates(x, y):
+    if x > 0 and y > 0:
+        return 1
+    elif x < 0 and y > 0:
+        return 2
+    elif x < 0 and y < 0:
+        return 3
+    elif x > 0 and y < 0:
+        return 4
+    else:
+        return "Похоже, введены координаты 0,0"
+
+
+f = checkCoordinates(x, y)
+print(f)
